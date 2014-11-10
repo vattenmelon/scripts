d663e9c999597ca8952dcdbf949b92c681e0f074
Erling Reizer <reizer@gmail.com>
2014-11-10 18:18:21 +0100

    .

--- src/spor.py
+++ src/spor.py
@@ -1,14 +1,20 @@
 #!/usr/local/bin/python
 import requests, sys, re
 
-if len(sys.argv) == 1:
-    print "oppgi sporingsnummer!"
+if len(sys.argv) != 2:
+    print "Oppgi ett sporingsnummer!"
     sys.exit()
     
 
 github_url = "http://sporing.bring.no/sporing.json?q="+sys.argv[1]
 r = requests.get(github_url)
 data = r.json()
-print "sporingsnummer " + sys.argv[1]
+#print data
+
+if 'error' in data['consignmentSet'][0]:
+    print 'Fant ikke pakke'
+    sys.exit()
+
+print "======== " + sys.argv[1] + " ========"
 for z in data['consignmentSet'][0]['packageSet'][0]['eventSet']:
     print z['displayDate'] + ' ' + z['displayTime'] + ' ' + re.sub('<[^<]+?>', '', z['description']) + " (" + z['city'] + ' ' + z['country'] + ")"
