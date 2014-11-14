724cd2d2ae3914e86c08d83c2f42904ea1607cf1
Erling Reizer <reizer@gmail.com>
2014-11-14 21:55:00 +0100

    added iss.py

---
+++ src/iss.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+import requests, sys, re
+
+CARDNUMBER = '' 
+PIN = '' 
+
+URL = "http://icare.myissworld.net/loginAction.do"
+QUERYPARAMS = "?requiresPIN=true&redirectUrl=&iCardNumber={0}&styleDir=&PIN={1}&language="
+
+if CARDNUMBER == '' or PIN == '':
+    print 'Sett kortnummer og pin i scriptet'
+    sys.exit(1)
+
+FORMATTED_URL = URL + QUERYPARAMS.format(CARDNUMBER, PIN)
+
+#print FORMATTED_URL
+
+r = requests.get(FORMATTED_URL)
+htmltext = r.text
+#print htmltext
+m = re.search('CafeN SITS Debet.*</a>.*<!--([0-9]*[.][0-9]*) -->', htmltext, re.DOTALL)
+numberAsString = m.group(1)
+print 'Kr ' + '{0:.2f}'.format(float(numberAsString))
