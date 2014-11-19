0c6fe10812480ce6fd6cc0fe26d4f5f70e089200
Erling Reizer <reizer@gmail.com>
2014-11-19 22:34:28 +0100

    added lotto.py

---
+++ src/lotto.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+import requests, sys, json
+from datetime import datetime
+
+URL = "https://www.norsk-tipping.no/api-lotto/getResultInfo.json"
+
+r = requests.get(URL)
+data = r.text
+data = data.replace("while(true);/* 0;", "")
+data = data.replace("/* */","")
+data = data.strip()
+json = json.loads(data)
+date = datetime.strptime(json['drawDate'], '%Y,%m,%d,%H,%M,%S')
+print 'Dato         ' + date.strftime('%d. %b %Y - %H.%M.%S') 
+print 'Rekke        ' + ",".join("{0}".format(n) for n in (json['mainTable']))
+print 'Tilleggstall ' + ",".join("{0}".format(n) for n in (json['addTable']))
+for i in range (0, len(json['prizeTable'])):
+    print '{:12s}: {:8d}'.format(json['prizeCaptionTable'][i], int(json['prizeTable'][i])) 
