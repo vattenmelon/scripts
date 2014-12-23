11b8d5cd85436597ed24cf307c5e2bb2eadaab37
Erling Reizer <reizer@gmail.com>
2014-12-23 19:29:49 +0100

    valuta.py fix

--- src/valuta.py
+++ src/valuta.py
@@ -4,10 +4,10 @@ import requests, sys, re
 if len(sys.argv) != 2:
     print "Oppgi en valuta"
     sys.exit()
-
-url = "http://www.freecurrencyconverterapi.com/api/v2/convert?q="+sys.argv[1]+"_NOK&compact=y"
+valuta = sys.argv[1].upper()
+url = "http://www.freecurrencyconverterapi.com/api/v2/convert?q="+valuta+"_NOK&compact=y"
 r = requests.get(url)
 data = r.json()
 #print data
 
-print data[sys.argv[1]+'_NOK']['val']
+print data[valuta+'_NOK']['val']
